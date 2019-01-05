e67a600cf7b2f4e58c01182b2a6b4422f0c28796
Unknown <amal95perera@gmail.com>
2019-01-05 17:05:30 +0530

    Add basic functionalities of destructable items

--- Assets/Shared/Destructable.cs
+++ Assets/Shared/Destructable.cs
@@ -4,13 +4,54 @@ using UnityEngine;
 
 public class Destructable : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] float hitPoints;
+
+    public event System.Action OnDeath;
+    public event System.Action OnDamageReceived;
+
+    float damageTaken;
+
+    public float HitPointsRemaining
+    {
+        get
+        {
+            return hitPoints - damageTaken;
+        }
+    }
+
+    public bool IsAlive
+    {
+        get
+        {
+            return HitPointsRemaining > 0;
+        }
+    }
+
+    public virtual void Die()
+    {
+        if (!IsAlive)
+            return;
+        if (OnDeath != null)
+            OnDeath();
+    }
+
+    public virtual void TakeDamage(float amount)
+    {
+        damageTaken += amount;
+
+        if (OnDamageReceived != null)
+        {
+            OnDamageReceived();
+        }
+
+        if (HitPointsRemaining <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Reset()
+    {
+        damageTaken = 0;
+    }
 }
