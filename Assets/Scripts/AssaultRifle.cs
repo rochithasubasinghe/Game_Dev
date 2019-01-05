18d585f57b99da13161112a0da60fd46673c0810
Unknown <amal95perera@gmail.com>
2019-01-05 12:38:45 +0530

    basic shooting function ok

--- Assets/Scripts/AssaultRifle.cs
+++ Assets/Scripts/AssaultRifle.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 
 public class AssaultRifle : Shooter {
 
+    public override void Fire()
+    {
+        base.Fire();
 
+        if (canFire)
+        {
+            //we fire the gun
+        }
+    }
 }
--- Assets/Shared/Shooter.cs
+++ Assets/Shared/Shooter.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Shooter : MonoBehaviour {
 
     [SerializeField] float rateOfFire;
+    [SerializeField] Projectile projectile;
     [HideInInspector]
     public Transform muzzel;
 
     float nextFireAllowed;
-    bool canFire;
+    public bool canFire;
 
     void Awake()
     {
@@ -26,6 +27,9 @@ public class Shooter : MonoBehaviour {
             return;
 
         nextFireAllowed = Time.time + rateOfFire;
+
+        //instantiate the projectile;
+        Instantiate(projectile, muzzel.position, muzzel.rotation);
 
         canFire = true;
     }
