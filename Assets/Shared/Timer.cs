37abae936379ec176479190c89185ce2b8549f7e
Unknown <amal95perera@gmail.com>
2019-01-05 19:52:47 +0530

    implement  basic functionality of the timer

--- Assets/Shared/Timer.cs
+++ Assets/Shared/Timer.cs
@@ -4,13 +4,43 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour {
 
+    private class TimedEvent
+    {
+        public float TimeToExecute;
+        public Callback Method;
+     }
+
+    private List<TimedEvent> events;
+    public delegate void Callback();
+
+  
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        events = new List<TimedEvent>();
 	}
 	
+    public void Add(Callback method,float inSeconds)
+    {
+        events.Add(new TimedEvent
+        {
+            Method = method,
+            TimeToExecute = Time.time + inSeconds
+        });
+    }
+
 	// Update is called once per frame
 	void Update () {
-		
+        if (events.Count == 0)
+            return;
+
+        for(int i = 0; i < events.Count; i++)
+        {
+            var timedEvent = events[i];
+            if (timedEvent.TimeToExecute <= Time.time)
+            {
+                timedEvent.Method();
+                events.Remove(timedEvent);
+            }
+        }
 	}
 }
