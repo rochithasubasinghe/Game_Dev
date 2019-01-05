4c31e4004d19257cce934a895b464f69a3cc0606
Unknown <amal95perera@gmail.com>
2019-01-05 17:27:00 +0530

    Add health to the cube and check the decreasing of its health

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Health : MonoBehaviour {
+public class Health : Destructable {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public override void Die()
+    {
+        base.Die();
+
+        print("we died");
+    }
+
+    public override void TakeDamage(float amount)
+    {
+        
+        base.TakeDamage(amount);
+        print("Remaining: " + HitPointsRemaining);
+    }
 }
