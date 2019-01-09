9352229f3285262895f1ae38d28e7e7ea983eef7
Unknown <amal95perera@gmail.com>
2019-01-10 00:32:07 +0530

    create Container.cs to handle the functionalities of the container

--- Assets/Shared/Container.cs
+++ Assets/Shared/Container.cs
@@ -1,16 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System;
 
 public class Container : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	private class ContainerItem
+    {
+        public System.Guid Id;
+        public string Name;
+        public int Maximum;
+
+        private int amountTaken;
+
+        public ContainerItem()
+        {
+            Id = System.Guid.NewGuid();
+        }
+
+        public int Remaining
+        {
+            get
+            {
+                return Maximum - amountTaken;
+            }
+        }
+
+        public  int Get(int value)
+        {
+            if((amountTaken + value) > Maximum)
+            {
+                int toMuch = (amountTaken + value) - Maximum;
+                amountTaken = Maximum;
+                return value - toMuch;
+            }
+
+            amountTaken += value;
+            return value;
+        }
+    }
+
+    List<ContainerItem> items;
+
+    void Awake()
+    {
+        items = new List<ContainerItem>();
+    }
+
+    public System.Guid Add(string name,int maximum)
+    {
+        items.Add(new ContainerItem
+        {
+            Maximum = maximum,
+            Name = name
+            
+        });
+        return items.Last().Id;
+    }
+
+    public int TakeFromContainer(System.Guid id, int amount)
+    {
+        var containerItem = items.Where(x => x.Id == id).FirstOrDefault();
+        if (containerItem == null)
+            return -1;
+        return containerItem.Get(amount);
+    }
 }
