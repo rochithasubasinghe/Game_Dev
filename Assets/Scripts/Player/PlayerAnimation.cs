e5007d7898221688a48b278f47812170e8b1c229
Kalana <edata.incore@gmail.com>
2019-01-09 10:10:58 +0530

    add PlayerAnimation Script  to handle player animation

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+    Animator animator;
+	// Use this for initialization
+	void Awake () {
+        animator = GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        animator.SetFloat("Vertical", GameManager.Instance.InputController.Vertical);
+        animator.SetFloat("Horizontal", GameManager.Instance.InputController.Horizontal);
+    }
+}
