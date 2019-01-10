17f911327ce27003420df1981d2b19aed03260b7
Unknown <amal95perera@gmail.com>
2019-01-11 01:01:38 +0530

    implemented basic functions in AudioController

--- Assets/Shared/AudioController.cs
+++ Assets/Shared/AudioController.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class AudioController : MonoBehaviour {
 
+    [SerializeField] AudioClip[] clips;
+    [SerializeField] float delayBetweenClips;
+
+    bool canPlay;
+    AudioSource source;
 	// Use this for initialization
 	void Start () {
-		
+        source = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	public void play () {
+
+        if (!canPlay)
+            return;
+
+        GameManager.Instance.Timer.Add(() =>
+        {
+            canPlay = true;
+        }, delayBetweenClips);
+
+        canPlay = false;
+
+        AudioClip clip = clips[Random.Range(0, clips.Length)];
+        source.PlayOneShot(clip);
 	}
 }
